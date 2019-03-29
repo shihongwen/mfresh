5853349f09c428992ddd82ba5e50af7c94c7e2fd
shihongwen <shi_hongwen@163.com>
2019-03-30 00:57:43 +0800

    Rev2:add dist/

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http=require('http');
 const express=require('express');
+const path = require('path');
 //服务器
 let app=express();
-http.createServer(app).listen(8080);
+http.createServer(app).listen(5050);
 
 // 中间件
 let bodyParser=require('body-parser');
 let cookieParser=require('cookie-parser');
 let cors=require('cors');
+app.use(express.static(path.join(__dirname,'dist')));
 app.use(bodyParser({extended:false}));
 app.use(cookieParser());
 app.use(cors({

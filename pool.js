be2d869647329e27f24d4bf0a7defa1aec7673c7
shihongwen <shi_hongwen@163.com>
2019-03-30 01:05:21 +0800

    Rev3: update pool.js

--- pool.js
+++ pool.js
@@ -1,10 +1,15 @@
 const mysql=require('mysql');
 let pool=mysql.createPool({
-  user:'root',
-  password:'',
-  database:'mfresh',
+//  user:'root',
+//  password:'',
+//  database:'mfresh',
+//  port:3306,
+//  host:'127.0.0.1',
+	user:'m3jxn1nwy3',
+  password:'kih3h3mmzxh2yzkkllhjm4jh0zmky53iljilz5wy',
+  database:'app_mfresh',
   port:3306,
-  host:'127.0.0.1',
+  host:'w.rdc.sae.sina.com.cn',
   connectionLimit:10
 });
 module.exports=pool;

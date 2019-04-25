d0be4546a220ef7e6bc6d6c4959045b124fe227e
shihongwen <shi_hongwen@163.com>
2019-04-25 22:44:58 +0800

    push to git

--- pool.js
+++ pool.js
@@ -1,15 +1,15 @@
 const mysql=require('mysql');
 let pool=mysql.createPool({
-//  user:'root',
-//  password:'',
-//  database:'mfresh',
-//  port:3306,
-//  host:'127.0.0.1',
-	user:'m3jxn1nwy3',
-  password:'kih3h3mmzxh2yzkkllhjm4jh0zmky53iljilz5wy',
-  database:'app_mfresh',
+  user:'root',
+  password:'',
+  database:'mfresh',
   port:3306,
-  host:'w.rdc.sae.sina.com.cn',
+  host:'127.0.0.1',
+//	user:'m3jxn1nwy3',
+//  password:'kih3h3mmzxh2yzkkllhjm4jh0zmky53iljilz5wy',
+//  database:'app_mfresh',
+//  port:3306,
+//  host:'w.rdc.sae.sina.com.cn',
   connectionLimit:10
 });
 module.exports=pool;
